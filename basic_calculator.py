887d204fa880ed3d865b999ef38ae11ee73166e4
TLALENG KESWA <116955530+Bongiey@users.noreply.github.com>
2025-03-16 23:43:00 +0200

    Add files via upload

---
+++ basic_calculator.py
@@ -0,0 +1,28 @@
+#Basic Calculator Program
+#Create a simple Python program that asks the user to input two numbers and a mathematical operation (addition, subtraction, multiplication, or division).
+#Perform the operation based on the user's input and print the result.
+#Example: If a user inputs 10, 5, and +, your program should display 10 + 5 = 15.
+#Get User input
+num1 = float (input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation(+, -, *, /): ")
+
+#Perform calculation based on the operation
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1}+{num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1}-{num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1}*{num2} = {result}")
+elif operation == "/":
+ if num2 != 0:
+    result = num1 /num2 
+    print(f"{num1}/{num2} = {result}")
+ else:
+    print("Error:Division by zero is not allowed.")
+else:
+ print("Invalid operation. Please enter one of (+, -, *, /)")
+
